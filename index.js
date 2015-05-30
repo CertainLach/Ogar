b0b0b781258959e52738ae2b18953d9b623663ab
Creeplays <Creeplays@users.noreply.github.com>
2015-05-30 20:15:28 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-var runMaster = false;
-var runGame = true;
-var mode;
 var GameMode = require('./gamemodes');
+
+
+var runMaster = false;
+var runGame = false;
+var mode;
+runMaster = true;
+runGame = true;
 mode = new GameMode.FFA();
-var GameServer = require('./GameServer');
-var game = new GameServer(443,mode);
-game.start();
+
+
+if (runMaster) {
+    // Initialize the master server
+    var MasterServer = require('./MasterServer');
+    var master = new MasterServer(80);
+    master.start();
+}
+
+if (runGame) {
+    // Initialize the game server
+    var GameServer = require('./GameServer');
+    var game = new GameServer(443,mode);
+    game.start();
+}
